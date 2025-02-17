8fcc1419d051f1f3da0fd0224b8edfd30933cd68
valens <ukwitegetsev9@gamail.com>
2025-02-17 14:05:36 +0200

    updating some files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import TaskList from './components/TaskList'; 
-import TaskCard from './components/TaskCard';
 import TaskForm from './components/TaskForm';
 import TaskItem from './components/TaskItem';
 import FilterSort from './components/FilterSort';
+import TaskCard from './components/TaskCard';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-3xl font-bold">Task Manager</h1>
-      </header>
-      <main className="p-4">
-        <TaskList />
-      </main>
+    <div>
+      <TaskForm />
+      <TaskItem />
+      <FilterSort />
+      <TaskCard />
     </div>
   );
-;
+};
 
 export default App;
